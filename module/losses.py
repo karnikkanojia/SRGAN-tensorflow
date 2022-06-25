43e41fd1518aa11ee9f50b1e97695aa4b2ee2ee4
karnikkanojia <karnikk1406120@gmail.com>
2022-06-25 11:20:54 +0530

    Added the custom loss class with MSE and Crossentropy loss methods

--- module/losses.py
+++ module/losses.py
@@ -1,1 +1,31 @@
 import tensorflow as tf
+from tf.keras.losses import MeanSquaredError
+from tf.keras.losses import BinaryCrossEntropy
+from tf.keras.losses import Reduction
+from tensorflow import reduce_mean
+
+
+class Losses:
+    def __init__(self, numReplicas):
+        self.numReplicas = numReplicas
+
+    def bce_loss(self, real, pred):
+        # Compute binary cross entropy loss without reduction
+        BCE = BinaryCrossEntropy(reduction=Reduction.NONE)
+        loss = BCE(real, pred)
+
+        # Compute reduced mean over the entire batch
+        loss = reduce_mean(loss)*(1.0/self.numReplicas)
+
+        # Return reduced bce loss
+        return loss
+
+    def mse_loss(self, real, pred):
+        # Compute mean squared error loss without reduction
+        MSE = MeanSquaredError(reduction=Reduction.NONE)
+
+        # Comptue reduced mean over the entire batch
+        loss = reduce_mean(MSE(real, pred))*(1.0/self.numReplicas)
+
+        # Return reduced mse loss
+        return loss
